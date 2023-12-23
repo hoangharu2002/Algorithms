9675414f4e053be13c7ba6e80cd297018b93da20
Just4Func <nnhoang2002@gmail.com>
2023-12-23 15:07:09 +0700

    tail recursive op and non-op

--- calculate factorial number/.cpp
+++ calculate factorial number/.cpp
@@ -2,6 +2,7 @@
 
 unsigned int fact(unsigned int);
 unsigned int fact1(unsigned int, unsigned int = 1);
+unsigned int fact2(unsigned int);
 
 int main()
 {
@@ -17,15 +18,24 @@ unsigned int fact(unsigned int n)
     return fact1(n);
 }
 
-// method 1: calculate factorial number with tail recursion optimization
+// ver 1: tail recursive optimized
 unsigned int fact1(unsigned int n, unsigned int f)
 {
 start:
     if (n <= 1)
         return f;
-    
+
     // return fact1(n - 1, f * n);  non-optimized
 
-    f *= n--;       // optimized by compiler
-    goto start;     //
+    f *= n--;   // optimized by compiler
+    goto start; //
+}
+
+// ver 2: tail recursive non-optimized
+unsigned int fact1(unsigned int n, unsigned int f)
+{
+    if (n <= 1)
+        return f;
+
+    return fact1(n - 1, f * n);
 }
