36e32ff3c1284cf848dc44d35a35ffbd36147ed4
Just4Func <nnhoang2002@gmail.com>
2023-12-25 19:35:19 +0700

    add ver 2

--- fibonacci/.cpp
+++ fibonacci/.cpp
@@ -37,3 +37,15 @@ unsigned int fibo1(unsigned int n)
 
     return num2;
 }
+
+// ver 2: using loop without using array
+unsigned int fibo2(unsigned int n)
+{
+    fi[0] = 0;
+    fi[1] = 1;
+    for (int i = 2; i < n; ++i)
+    {
+        fi[i] = fi[i - 1] + fi[i - 2];
+    }
+    return fi[n - 1];
+}
